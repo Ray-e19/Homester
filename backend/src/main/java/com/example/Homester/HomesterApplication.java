3d8e236c370c91dd475f50946e687e4bbec5942d
Raymond Embrador <raymondembrador@Raymonds-MBP.attlocal.net>
2025-03-20 00:20:05 -0700

    initialized backend

---
+++ backend/src/main/java/com/example/Homester/HomesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.Homester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomesterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Homester/HomesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Homester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
